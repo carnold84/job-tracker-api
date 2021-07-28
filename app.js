8a30caaba83e77616112c9cb5e9ec42224f0ed37
carnold84 <carnold84@users.noreply.github.com>
2021-07-28 16:25:35 +1200

    Updates

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const routes = {
 };
 
 const app = express();
+const PATH = process.env.APP_PATH ? `/${process.env.APP_PATH}` : '';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -35,44 +36,39 @@ function makeHandlerAwareOfAsyncErrors(handler) {
 }
 
 // We provide a root route just as an example
-app.get('/', (req, res) => {
-  res.send(`
-		<h2>Hello, Sequelize + Express!</h2>
-		<p>Make sure you have executed <b>npm run setup-example-db</b> once to have a populated example database. Otherwise, you will get <i>'no such table'</i> errors.</p>
-		<p>Try some routes, such as <a href='/api/users'>/api/users</a> or <a href='/api/orchestras?includeInstruments'>/api/orchestras?includeInstruments</a>!</p>
-		<p>To experiment with POST/PUT/DELETE requests, use a tool for creating HTTP requests such as <a href='https://github.com/jakubroztocil/httpie#readme'>HTTPie</a>, <a href='https://www.postman.com/downloads/'>Postman</a>, or even <a href='https://en.wikipedia.org/wiki/CURL'>the curl command</a>, or write some JS code for it with <a href='https://github.com/sindresorhus/got#readme'>got</a>, <a href='https://github.com/sindresorhus/ky#readme'>ky</a> or <a href='https://github.com/axios/axios#readme'>axios</a>.</p>
-	`);
+app.get(`${PATH}`, (req, res) => {
+  res.send(`<p>Access Denied</p>`);
 });
 
 // We define the standard REST APIs for each route (if they exist).
 for (const [routeName, routeController] of Object.entries(routes)) {
   if (routeController.getAll) {
     app.get(
-      `/api/${routeName}`,
+      `${PATH}/api/${routeName}`,
       makeHandlerAwareOfAsyncErrors(routeController.getAll)
     );
   }
   if (routeController.getById) {
     app.get(
-      `/api/${routeName}/:id`,
+      `${PATH}/api/${routeName}/:id`,
       makeHandlerAwareOfAsyncErrors(routeController.getById)
     );
   }
   if (routeController.create) {
     app.post(
-      `/api/${routeName}`,
+      `${PATH}/api/${routeName}`,
       makeHandlerAwareOfAsyncErrors(routeController.create)
     );
   }
   if (routeController.update) {
     app.put(
-      `/api/${routeName}/:id`,
+      `${PATH}/api/${routeName}/:id`,
       makeHandlerAwareOfAsyncErrors(routeController.update)
     );
   }
   if (routeController.remove) {
     app.delete(
-      `/api/${routeName}/:id`,
+      `${PATH}/api/${routeName}/:id`,
       makeHandlerAwareOfAsyncErrors(routeController.remove)
     );
   }
--- sequelize/index.js
+++ sequelize/index.js
@@ -1,16 +1,11 @@
 const { Sequelize } = require('sequelize');
 
-const {
-  DATABASE_HOST,
-  DATABASE_NAME,
-  DATABASE_PASS,
-  DATABASE_PORT,
-  DATABASE_USER,
-} = process.env;
+const { DATABASE_NAME, DATABASE_PASS, DATABASE_PORT, DATABASE_USER } =
+  process.env;
 
 const sequelize = new Sequelize(DATABASE_NAME, DATABASE_USER, DATABASE_PASS, {
   dialect: 'mysql',
-  host: DATABASE_HOST,
+  host: 'localhost',
   port: DATABASE_PORT,
 });
 
